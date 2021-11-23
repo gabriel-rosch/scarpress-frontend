6124ecdaba20afc0ac6254d08034e71162c805b4
Your Name <gabrielrosch@gmail.com  git config --global user.name gabriel-rosch>
2021-11-23 13:36:11 -0300

    Modal Transactions

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { GlobalStyle } from "./styles/global";
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { useState } from 'react';
+import Modal from 'react-modal'
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransitionModalOpen, setIsNewTransitionModalOpen] = useState(false);
+
+  function handleOpenNewTransitionModal() {
+    setIsNewTransitionModalOpen(true);
+  }
+
+  function handleCloseNewTransitionModal() {
+    setIsNewTransitionModalOpen(false);
+  }
   return (
     <>
-      <Header/>
-      <Dashboard/>
-      <GlobalStyle/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransitionModal}/>
+      <Dashboard />
+      
+      <NewTransactionModal isOpen={isNewTransitionModalOpen} onRequestClose={handleCloseNewTransitionModal}/>
+
+      <GlobalStyle />
     </>
   );
 }
-
